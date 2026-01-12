d4869153d9a19d66083166cc2f813dce45acb79b
Dharmgora.23@gmail.com <Dharmgora.23>
2026-01-12 13:07:34 +0530

    add model to main file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// const userModel = require("./models/user-model");
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(3000);
