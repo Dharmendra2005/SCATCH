69531d930387be226c65b2c2e4887234639964db
Dharmgora.23@gmail.com <Dharmgora.23>
2026-01-12 19:09:29 +0530

    create MAIN model which connected to all other models

---
+++ config/mongoose-connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/Scatch")
+.then(function(){
+    console.log("Connected");
+})
+.catch(function(err){
+    console.log(err);
+})
+
+module.exports = mongoose.connection;
