616e87a7d2fdf908e2abd142c4fd951cf0998272
Dharmgora.23@gmail.com <Dharmgora.23>
2026-01-13 18:33:40 +0530

    required all routers

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const userModel = require("./models/user-model");
 const cookieParser = require('cookie-parser');
 const db = require("./config/mongoose-connection");
 const ownersRouter = require("./routes/ownersRouter");
