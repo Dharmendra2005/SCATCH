d60545c1ffe5b960f2adbbebe338960fe9335d3c
Dharmgora.23@gmail.com <Dharmgora.23>
2026-01-14 23:40:37 +0530

    split the register and login part

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,14 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController");
+
 
 router.get("/", (req, res) => {
-    res.send("hello Dharm U r doing good job till now !")
-})
+  res.send("hello Dharm U r doing good job till now !");
+});
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
 
 module.exports = router;
