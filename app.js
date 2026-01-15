73d57893903c2f4f507cd80b1b22e3ce46fa4daa
Dharmgora.23@gmail.com <Dharmgora.23@gmail.com>
2026-01-16 02:31:16 +0530

    update

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+process.noDeprecation = true;
+
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const flash = require('express-flash');
 const db = require("./config/mongoose-connection");
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
-require("dotenv").config();
+const indexRouter = require("./routes/index");
+
+const expressSession = require('express-session');
 
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.locals.loggedin = !!req.cookies.token;
+  next();
+});
+app.use(
+    expressSession({
+        resave:false,
+        saveUninitialized: false,
+        secret: process.env.JWT_KEY || "shhhhhhhhhhhhhh",
+    })
+);
+app.use(flash());
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -18,10 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+app.use("/", indexRouter);
 
-app.get("/", (req, res) => {
-    res.render("index");
-})
-
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
