d10ebb473a05e7322bd1ed9afdb89e6ac03e1029
Dharmgora.23@gmail.com <Dharmgora.23@gmail.com>
2026-01-16 02:38:17 +0530

    token generataed

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 
+const JWT_KEY = process.env.JWT_KEY || "shhhhhhhhhhhhhh";
 
 const generateToken = (user) => {
-    return jwt.sign({email: user.email, id: user._id}, process.env.JWT_KEY);
+    return jwt.sign({email: user.email, id: user._id}, JWT_KEY, { expiresIn: "1h" });
 }
+
 module.exports.generateToken = generateToken;
