fe59b50686b0f67ba41684ed47454ad1d577e9e5
Dharm <Dharmgora.23@gmail.com>
2026-01-19 00:30:10 +0530

    update owner login process

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,40 +6,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const isOwnerLoggedIn = require("../middlewares/isOwnerLoggedIn");
 
-if (process.env.NODE_ENV === "development") {
-  router.post("/create", async (req, res) => {
-    let owners = await ownerModel.find();
-    if (owners.length > 0) {
-      return res.status(504).send("you have not permission to create product");
-    }
-    let { fullname, email, password } = req.body;
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        let createdOwner = await ownerModel.create({
-          fullname,
-          email,
-          password: hash,
-        });
-        let token = jwt.sign({ email }, process.env.JWT_KEY || "shhhhhhhhhhhhhh");
-        res.cookie("token", token);
-        res.status(201).send(createdOwner);
-      });
-    });
-    // console.log(process.env.NODE_ENV) //to check node environment
-  });
-}
-
-router.get("/admin", isOwnerLoggedIn, async (req, res) => {
-  try {
-    let success = req.flash("success");
-    let products = await productModel.find().sort({ createdAt: -1 });
-    res.render("createproducts", { success, products });
-  } catch (error) {
-    console.error("Admin panel error:", error);
-    res.render("createproducts", { success: [], products: [] });
-  }
-});
-
 // Delete all products
 router.delete("/delete/all", async (req, res) => {
   try {
