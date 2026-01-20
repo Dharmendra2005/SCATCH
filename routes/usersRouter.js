91e7ac6d51f5e89affa9ef1e4a217d7285e26b0e
Dharm <Dharmgora.23@gmail.com>
2026-01-20 20:27:45 +0530

    create new routers

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logout } = require("../controllers/authController");
+const userModel = require("../models/user-model");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 
-const isLoggedIn = require('../middlewares/isLoggedIn');
-
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
 // router.get("/", (req, res) => {
 //   res.send("hello Dharm U r doing good job till now !");
@@ -11,10 +15,23 @@ const isLoggedIn = require('../middlewares/isLoggedIn');
 
 router.post("/register", registerUser);
 
-
 router.post("/login", loginUser);
 
 router.get("/logout", logout);
 
+router.delete("/delete/:id", isLoggedIn, async (req, res) => {
+  try {
+    const userID = req.user._id;
+    const productId = req.params.id;
+    // console.log(productId);
+    await userModel.updateOne({_id : userID}, {$pull:{cart:productId}});
+    res.json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    // console.error("Delete product error:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to delete product" });
+  }
+});
 
 module.exports = router;
