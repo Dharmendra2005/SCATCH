6226e52044773cf079f818282c57c4eb1a275ca8
Dharm <Dharmgora.23@gmail.com>
2026-01-23 20:59:37 +0530

    Created new router

--- routes/addressRouter.js
+++ routes/addressRouter.js
@@ -1,27 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user-model");
 const addressModel = require("../models/address-model");
 const isLoggedIn = require("../middlewares/isLoggedIn");
 
-router.get("/", isLoggedIn, async (req, res) => {
-  let addresses = await addressModel.find({user: req.user._id});
-  res.render("selectedAddresses", {addresses});
+router.post("/address", isLoggedIn, async (req, res) => {
+  console.log("hiii..")
+  res.redirect("/address/placeorder");
+});
+
+
+router.get("/placeorder", isLoggedIn, async (req, res) => {
+  const addresses = await addressModel
+    .find({ user: req.user._id })
+    .sort({ defaultAddress: -1, createdAt: 1 });
+  const user = await userModel.findById(req.user._id).populate("cart");
+  // console.log(user, addresses);
+  res.render("placeOrder", { cartItems: user.cart, addresses });
 });
 
 router.post("/collectAddress", isLoggedIn, async (req, res) => {
   try {
-    let { name, number, pincode, house, street, addtype, defaultAddress } = req.body;
+    let { name, number, pincode, house, street, addtype, defaultAddress } =
+      req.body;
+    if (defaultAddress) {
+      await addressModel.updateMany(
+        { user: req.user._id },
+        { $set: { defaultAddress: false } },
+      );
+    }
     let address = await addressModel.create({
-      user: req.user._id,  
+      user: req.user._id,
       name,
       Mobile_no: number,
       PinCode: pincode,
       House_no: house,
       Address: street,
       addressType: addtype,
-      defaultAddress: defaultAddress ? true : false
+      defaultAddress: defaultAddress ? true : false,
     });
-    res.redirect("/address");
+
+    await userModel.findByIdAndUpdate(req.user._id, {
+      $push: { Address: address._id },
+    });
+
+    res.redirect("/address/placeorder");
   } catch (err) {
     console.error(err.message);
   }
