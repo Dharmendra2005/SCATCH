aad086945143388b4c446c2b13263101251825ba
Dharm <Dharmgora.23@gmail.com>
2026-01-25 21:17:20 +0530

    Worked on Payment through COD

--- routes/CODRouter.js
+++ routes/CODRouter.js
@@ -1,19 +1,32 @@
-const express = require('express');
+const express = require("express");
+const addressModel = require("../models/address-model");
+const isLoggedIn = require("../middlewares/isLoggedIn");
+const userModel = require("../models/user-model");
 
 const router = express.Router();
 
+router.get("/orders-place-cod", isLoggedIn, async (req, res) => {
+  try {
+    const userid = req.user._id;
+    const address = await addressModel.findById(userid);
+    // if (!address) return res.status(400).send("Address not found");
+    const newOrder = {
+      items: req.user.cart,           // all cart items
+      address: address,           // address ID
+      paymentMethod: "COD",
+      createdAt: new Date(),
+    };
+    // console.log(newOrder);
+    await userModel.findByIdAndUpdate(userid, {
+      $push: { orders: newOrder },
+      $set: { cart: [] }, // this clears cart
+    });
+    res.render("orderSuccess", {address});
 
-
-router.get('/orders/place-cod', async (req, res) => {
-    try {
-        // 1. Get the user's cart items from DB/session
-        // 2. Create a new order in DB with status "Pending COD"
-        // 3. Clear cart
-        res.redirect('/orders/confirmation'); // or wherever you show success
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Something went wrong");
-    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(501).send("Something went wrong");
+  }
 });
 
 module.exports = router;
