1d40af4c002f390536f18f908ed27ebdcf818971
Dharm <Dharmgora.23@gmail.com>
2026-01-27 02:35:53 +0530

    updated again

--- public/javascripts/profilePanel.js
+++ public/javascripts/profilePanel.js
@@ -1,9 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const profile = document.querySelector("#openProfileBtn");
-
-  if (!profile) return; // user not logged in or button not rendered
-
-  profile.addEventListener("click", () => {
-    console.log("Profile button clicked");
-  });
+const profile = document.querySelector("#openProfileBtn");
+profile.addEventListener("click", () => {
+  console.log("Profile button clicked");
 });
