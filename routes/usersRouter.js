54353aedcd44b3b9fc8812e6a6e37f0f140b4530
Dharm <Dharmgora.23@gmail.com>
2026-01-27 23:22:35 +0530

    updated

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -25,7 +25,10 @@ router.delete("/delete/:id", isLoggedIn, async (req, res) => {
     const userID = req.user._id;
     const productId = req.params.id;
     // console.log(productId);
-    await userModel.updateOne({_id : userID}, {$pull:{cart:productId}});
+    await userModel.updateOne(
+      { _id: userID },
+      { $pull: { cart: { product: productId } } },
+    );
     res.json({ success: true, message: "Product deleted" });
   } catch (error) {
     // console.error("Delete product error:", error);
@@ -35,11 +38,25 @@ router.delete("/delete/:id", isLoggedIn, async (req, res) => {
   }
 });
 
-
 router.get("/payment", isLoggedIn, async (req, res) => {
-  const user = await userModel.findById(req.user._id).populate('cart')
-  res.render("finallyPlaceOrder", {cartItems: user.cart});
-})
+  const user = await userModel.findById(req.user._id).populate("cart.product");
+
+  // Transform cart items to include product details with quantity
+  const cartItems =
+    user.cart.map((item) => ({
+      _id: item.product._id,
+      name: item.product.name,
+      price: item.product.price,
+      discount: item.product.discount,
+      image: item.product.image,
+      bgcolor: item.product.bgcolor,
+      panelcolor: item.product.panelcolor,
+      textcolor: item.product.textcolor,
+      quantity: item.quantity,
+    })) || [];
+
+  res.render("finallyPlaceOrder", { cartItems });
+});
 
 router.post("/cart/update", isLoggedIn, async (req, res) => {
   try {
@@ -47,8 +64,8 @@ router.post("/cart/update", isLoggedIn, async (req, res) => {
     const userId = req.user._id;
 
     await userModel.updateOne(
-      { _id: userId, "cart._id": productId },
-      { $set: { "cart.$.quantity": quantity } }
+      { _id: userId, "cart.product": productId },
+      { $set: { "cart.$.quantity": quantity } },
     );
 
     res.json({ success: true });
@@ -67,4 +84,14 @@ router.get("/profile", isLoggedIn, async (req, res) => {
   }
 });
 
+router.get("/myorders", isLoggedIn, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const addresses = await addressModel.find({ user: userId });
+    res.send("My address are there");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 module.exports = router;
