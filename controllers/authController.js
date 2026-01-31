e82f1164d0249f847a3b2dc2e1a3dafa50ad47cd
Dharm <Dharmgora.23@gmail.com>
2026-01-31 23:05:01 +0530

    update user process

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,16 @@ const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../utils/generateToken");
 
-
-
-
 const productModel = require("../models/product-model");
 
-module.exports.registerUser = async function (req, res){
+module.exports.registerUser = async function (req, res) {
   try {
     let { fullname, email, password } = req.body;
-    let user =  await userModel.findOne({email});
-    if(user) return res.status(401).send("you already have an account please do login.")
+    let user = await userModel.findOne({ email });
+    if (user)
+      return res
+        .status(401)
+        .send("you already have an account please do login.");
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(password, salt, async (err, hash) => {
         let user = await userModel.create({
@@ -28,29 +28,33 @@ module.exports.registerUser = async function (req, res){
   } catch (err) {
     console.log(err.message);
   }
-}
+};
 
-module.exports.loginUser = async function (req, res){
-    let {email, password}  = req.body;
+module.exports.loginUser = async function (req, res) {
+  let { email, password } = req.body;
 
-    let user = await userModel.findOne({email: email});
-    if(!user) {
-       req.flash("error", "Email or Password incorrect.");
-       return res.redirect("/");
+  let user = await userModel.findOne({ email: email });
+  if (!user) {
+    req.flash("error", "Email or Password incorrect.");
+    return res.redirect("/");
+  }
+
+  bcrypt.compare(password, user.password, async (err, result) => {
+    if (result) {
+      let token = generateToken(user);
+      res.cookie("token", token);
+      let products = await productModel.find().sort({ createdAt: -1 });
+      // Get cart product IDs from the user's cart
+      const cartProductIds = user.cart
+        ? user.cart.map((id) => id.toString())
+        : [];
+      res.render("shop", { products, cartProductIds });
+    } else {
+      req.flash("error", "Email or Password incorrect.");
+      return res.redirect("/");
     }
-
-    bcrypt.compare(password, user.password, async (err, result) => {
-        if(result){
-            let token = generateToken(user);
-            res.cookie("token", token);
-            let products = await productModel.find();
-            res.render("shop", { products });
-        }else{
-             req.flash("error", "Email or Password incorrect.");
-             return res.redirect("/");
-        }
-    })
-}
+  });
+};
 
 module.exports.logout = function (req, res) {
   res.clearCookie("token", { httpOnly: true }); // ensures cookie is removed
